72dd80211721b904bf01d5d9ad608522cb64af5b
GiandomenicoRiceputi <giandomenicoriceputi@gmail.com>
2024-01-05 20:55:16 +0100

    start from basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { Canvas } from "@react-three/fiber";
-import {OrbitControls} from "@react-three/drei";
+import {Canvas} from "@react-three/fiber";
 
 function App() {
-  return (
-    <Canvas
-      camera={{
-        position: [3, 3, 3],
-      }}
-    >
-        <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
-    </Canvas>
-  );
+    return (
+        <Canvas camera={{position: [0, 2, -6]}} shadows>
+            <mesh position={[-3, 0, 0]}>
+                <boxGeometry/>
+                <meshStandardMaterial color="red"/>
+            </mesh>
+            <mesh position={[0, 0, 0]}>
+                <boxGeometry/>
+                <meshStandardMaterial color="blue"/>
+            </mesh>
+            <mesh position={[3, 0, 0]}>
+                <boxGeometry/>
+                <meshStandardMaterial color="green"/>
+            </mesh>
+
+            <ambientLight intensity={0.5}/>
+            <directionalLight position={[0, 0, 3]} intensity={1}/>
+            <directionalLight position={[0, 3, 3]} intensity={0.5}/>
+        </Canvas>
+    );
 }
 
 export default App;
