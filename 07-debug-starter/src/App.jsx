880a1da2bee00854a41f8651aa013a44f1cdb593
GiandomenicoRiceputi <giandomenicoriceputi@gmail.com>
2024-01-06 17:31:07 +0100

    start debug

---
+++ 07-debug-starter/src/App.jsx
@@ -0,0 +1,22 @@
+import { Canvas } from "@react-three/fiber";
+
+const Box = () => {
+  return (
+    <mesh>
+      <boxGeometry />
+      <meshBasicMaterial color="white" transparent opacity={0} />
+    </mesh>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [3, 3, 3] }}>
+        <Box />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
